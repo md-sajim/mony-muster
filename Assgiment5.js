2632ab53f50501a500a855fdec97c6699a4e980c
md-sajim <mdabutaliveb@gmail.com>
2022-02-17 09:03:46 +0600

    push javaScripts

--- Assgiment5.js
+++ Assgiment5.js
@@ -1,1 +1,25 @@
-
+document
+  .getElementById("calculateButton")
+  .addEventListener("click", function () {
+    const foodFild = document.getElementById("foodFild");
+    const foodValu = foodFild.value;
+    const rentFild = document.getElementById("rentFild");
+    const rentValu = rentFild.value;
+    const clothesFild = document.getElementById("clothesFild");
+    const clothesValu = clothesFild.value;
+    const totalCost =
+      parseFloat(foodValu) + parseFloat(rentValu) + parseFloat(clothesValu);
+    console.log(totalCost);
+    const totalExpenses = document.getElementById("totalExpenses");
+    totalExpenses.innerText = totalCost;
+    const incomeFild = document.getElementById("incomeFild");
+    const incomeValu = parseFloat(incomeFild.value);
+    const netBalance = incomeValu - totalCost;
+    console.log(netBalance);
+    const carantbalance = document.getElementById("carantbalance");
+    carantbalance.innerText = netBalance;
+    const saveParcant = document.getElementById("saveParcant");
+    const parsentValu = parseFloat(saveParcant.value);
+    const parsentAmount = incomeValu / parsentValu;
+    console.log(parsentAmount);
+  });
